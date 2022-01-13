52cc3c8fa240cb3e9d6ecc67542eca1e775eac5c
ealic4 <ealic4@etf.unsa.ba>
2022-01-13 20:06:04 +0100

    Prva verzija

---
+++ skripta.js
@@ -0,0 +1,24 @@
+function OpenWindowLogin(){
+    document.getElementById("cijelilogin").style.display = "flex";
+}
+function CloseLogin(){
+    document.getElementById("cijelilogin").style.display = "none";
+}
+
+function OpenWindowRegistration(){
+    document.getElementById("cijelaregistracija").style.display = "flex";
+}
+function CloseRegistration(){
+    document.getElementById("cijelaregistracija").style.display = "none";
+}
+function OpenWindowReservation(){
+    document.getElementById("cijelarezervacija").style.display = "flex";
+}
+function CloseReservation(){
+    document.getElementById("cijelarezervacija").style.display = "none";
+}
+
+document.getElementById("ime").focus();
+
+
+
